dfb0e942e95b318e63e1c1e83ae1e02225e4eb47
Pedro Ballastreri <gballastreri@gmail.com>
2025-02-20 15:15:53 -0300

    construtores

---
+++ poo/src/cadastro/SistemaDeCadastro.java
@@ -0,0 +1,14 @@
+package cadastro;
+
+public class SistemaDeCadastro {
+    public static void main(String[] args) {
+		//criamos uma pessoa no sistema
+		Pessoa marcos = new Pessoa("234.554.678-63", "Marcos");
+		
+		//definimos o endereço de marcos
+		marcos.setEndereco("RUA DAS MARIAS");
+		
+		
+		System.out.println(marcos.getNome() + "-" + marcos.getCpf());
+	}
+}
